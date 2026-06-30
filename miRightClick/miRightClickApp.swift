8790cea0bba272f5008a74076709fd991deb8c0f
metin-aksu <drmetin@msn.com>
2026-06-30 03:58:18 +0300

    Initial Commit

---
+++ miRightClick/miRightClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  miRightClickApp.swift
+//  miRightClick
+//
+//  Created by Metin AKSU on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct miRightClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
